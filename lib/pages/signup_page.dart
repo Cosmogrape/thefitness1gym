addcd249602ed1d9179917608749aad76e0e926e
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-02 04:22:32 +0300

    rename home to be signup page

---
+++ lib/pages/signup_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:thefitness1gym/pages/widgets/signup_box.dart';
+
+import 'widgets/fitness1_title.dart';
+
+@immutable
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  bool nextEnabled = false;
+  bool splashEnd = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 1, milliseconds: 200), () {
+      setState(() => splashEnd = true);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const animationDuration = Duration(milliseconds: 500);
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          AnimatedOpacity(
+            opacity: splashEnd ? 0 : 1,
+            duration: animationDuration,
+            child: const Center(child: Fitness1Title()),
+          ),
+          AnimatedScale(
+            scale: splashEnd ? 1 : .95,
+            duration: animationDuration,
+            child: Center(
+              child: AnimatedOpacity(
+                opacity: splashEnd ? 1 : 0,
+                duration: animationDuration,
+                child: const SignupBox(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
