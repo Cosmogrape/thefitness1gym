de07a28d1436e980ed48f74cb30f43755a73d8d2
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-02 15:37:30 +0300

    feat: standard predefined sizes to be used across the app

---
+++ lib/pages/values/size.dart
@@ -0,0 +1,12 @@
+class PredefinedSize {
+  static const double _small = 5, _regular = 10, _medium = 15, _big = 20;
+
+  static double get paddingSmall => _small;
+  static double get padding => _regular;
+  static double get paddingMedium => _medium;
+  static double get paddingBig => _big;
+
+  static double get radiusSmall => _small;
+  static double get radius => _regular;
+  static double get radiusBig => _big;
+}
