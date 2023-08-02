04fcfaf6962d9a77aabdbadccb1666c9f71ae29f
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-02 04:09:04 +0300

    move signup widget into a separate file

---
+++ lib/pages/widgets/signup_box.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class SignupBox extends StatefulWidget {
+  const SignupBox({this.signin = false, super.key});
+
+  final bool signin;
+
+  @override
+  State<SignupBox> createState() => _SignupBoxState();
+}
+
+enum SignupStep {
+  name,
+  email,
+  password,
+}
+
+enum SigninStep {
+  email,
+  password,
+}
+
+class _SignupBoxState extends State<SignupBox> {
+  int index = 0;
+  bool nextEnabled = false;
+  bool get isLastStep => index == 2;
+  bool get isBackEnabled => index > 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    const animationDuration = Duration(milliseconds: 500);
+
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "Become a member",
+              style: theme.textTheme.headlineSmall!.copyWith(
+                color: theme.colorScheme.primary,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 16),
+            AnimatedSwitcher(
+              duration: animationDuration,
+              transitionBuilder: (child, animation) => FadeTransition(opacity: animation, child: child),
+              child: index == 1
+                  ? const Text("Enter your email")
+                  : index == 2
+                      ? const Text("Enter your password")
+                      : const Text("Enter your name"),
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                labelText: "Name",
+              ),
+              onChanged: (value) => setState(() => nextEnabled = value.isNotEmpty),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                AnimatedOpacity(
+                  opacity: isBackEnabled ? 1 : 0,
+                  duration: animationDuration,
+                  child: IconButton(
+                    style: TextButton.styleFrom(
+                      foregroundColor: theme.colorScheme.secondary,
+                    ),
+                    onPressed: isBackEnabled ? () => setState(() => index--) : null,
+                    icon: const Icon(FontAwesomeIcons.circleArrowLeft),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                TextButton.icon(
+                  icon: const Icon(FontAwesomeIcons.circleArrowRight),
+                  label: AnimatedSwitcher(
+                    duration: animationDuration,
+                    child: isLastStep ? const Text("Finish") : const Text(" Next "),
+                  ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: nextEnabled
+                        ? (isLastStep ? theme.colorScheme.primary : theme.colorScheme.secondary)
+                        : theme.colorScheme.secondary.withOpacity(.5),
+                    foregroundColor: isLastStep ? theme.colorScheme.onPrimary : theme.colorScheme.onSecondary,
+                  ),
+                  onPressed: nextEnabled ? () => setState(() => {if (!isLastStep) index++}) : null,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
