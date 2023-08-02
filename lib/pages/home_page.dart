0730de696ba2aabd718f6ba030a733ff431c22f4
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-02 15:39:33 +0300

    add: animated headline text with overview card

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:thefitness1gym/pages/widgets/headline_text.dart';
+import 'package:thefitness1gym/pages/widgets/overview_widget/overview.dart';
+
+import 'values/size.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
-  static const routeName = '/home_page';
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -13,12 +16,28 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const BackButton(),
-        title: const Text('Home Page'),
-      ),
-      body: const Center(
-        child: Text('Home Page'),
+      appBar: AppBar(toolbarHeight: 0),
+      body: Padding(
+        padding: EdgeInsets.all(PredefinedSize.paddingBig),
+        child: ListView(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const HeadlineText(),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(FontAwesomeIcons.user),
+                ),
+              ],
+            ),
+            SizedBox(height: PredefinedSize.paddingBig),
+            const Row(children: [
+              Overview(),
+              Overview(),
+            ]),
+          ],
+        ),
       ),
     );
   }
