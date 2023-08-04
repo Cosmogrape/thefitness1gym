0c1e5aca198d600aa657db62f169d07479fa16c2
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-04 18:09:12 +0300

    feat: splut predefiend_size into predefined_padding and predefined_radius

---
+++ lib/assets/values/predefined_padding.dart
@@ -0,0 +1,13 @@
+class PredefinedPadding {
+  static const double xTiny = 1;
+  static const double tiny = 2;
+  static const double xSmall = 3;
+  static const double small = 5;
+  static const double regular = 10;
+  static const double medium = 15;
+  static const double big = 20;
+  static const double large = 25;
+  static const double xLarge = 30;
+  static const double xxLarge = 35;
+  static const double huge = 40;
+}
