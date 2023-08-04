7284550aa6fde3de15b0b02de4f380b183d3401e
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-04 17:26:07 +0300

    feat: item for upcoming card for home page

---
+++ lib/widgets/home_widgets/upcoming_widget/upcoming_item.dart
@@ -0,0 +1,53 @@
+import 'package:date_time_format/date_time_format.dart';
+import 'package:flutter/material.dart';
+import 'package:thefitness1gym/assets/values/predefined_size.dart';
+
+class UpcomingItem extends StatelessWidget {
+  const UpcomingItem({
+    required this.text,
+    required this.date,
+    super.key,
+  });
+
+  final String text;
+  final DateTime? date;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    List<Widget> children = [];
+
+    if (date != null) {
+      children.add(
+        Text(
+          date!.format("d/m/y - h:m"),
+          style: theme.textTheme.labelSmall!.copyWith(
+            color: theme.colorScheme.onSurface,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      );
+    }
+    children.add(
+      Text(
+        text,
+        style: theme.textTheme.labelMedium!.copyWith(
+          color: theme.colorScheme.onBackground,
+          fontWeight: FontWeight.bold,
+        ),
+        overflow: TextOverflow.ellipsis,
+      ),
+    );
+
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: PredefinedSize.padding, vertical: PredefinedSize.paddingSmall),
+      color: Colors.black.withOpacity(.2),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: children,
+      ),
+    );
+  }
+}
