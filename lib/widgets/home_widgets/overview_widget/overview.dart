e2b4f95309bd5e9d1f29a3a416a01e3f3d4c8576
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-04 17:27:13 +0300

    update: use `HomeCard` for standardization

--- lib/widgets/home_widgets/overview_widget/overview.dart
+++ lib/widgets/home_widgets/overview_widget/overview.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:thefitness1gym/assets/values/predefined_size.dart';
-import 'package:thefitness1gym/global/widgets/animated_tap.dart';
 import 'package:thefitness1gym/widgets/home_widgets/overview_widget/overview_item.dart';
+
+import '../home_card.dart';
 
 class Overview extends StatefulWidget {
   const Overview({super.key});
@@ -12,11 +13,11 @@ class Overview extends StatefulWidget {
 }
 
 class _OverviewState extends State<Overview> {
-  final borderRadius = BorderRadius.circular(PredefinedSize.radiusMedium);
-
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+
+    final borderRadius = BorderRadius.circular(PredefinedSize.radiusMedium);
 
     final itemPadding = EdgeInsets.symmetric(
       horizontal: PredefinedSize.padding,
@@ -24,88 +25,77 @@ class _OverviewState extends State<Overview> {
     );
     const spacer = SizedBox(height: 2);
 
-    return Expanded(
-      child: AnimatedTap(
-        tapDownOpacity: .6,
-        onTap: () => Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => const Scaffold(body: Center(child: Text("Overview"))),
-          ),
-        ),
-        child: Card(
-          shape: RoundedRectangleBorder(borderRadius: borderRadius),
-          color: theme.colorScheme.primary,
-          child: Padding(
-            padding: EdgeInsets.all(PredefinedSize.paddingSmall),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                // Text(
-                //   "Overview",
-                //   style: theme.textTheme.titleMedium!.copyWith(
-                //     fontSize: 18,
-                //     color: theme.colorScheme.onPrimary,
-                //     fontWeight: FontWeight.bold,
-                //   ),
-                // ),
-                // SizedBox(height: PredefinedSize.padding),
-                ClipRRect(
-                  borderRadius: borderRadius,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      OverviewItem(
-                        textBig: "6",
-                        text: 'days streak',
-                        icon: FontAwesomeIcons.fire,
-                        color: theme.colorScheme.primary,
-                        backgroundColor: theme.colorScheme.background.withOpacity(.7),
-                        padding: itemPadding,
-                        onTap: () {},
-                      ),
-                      spacer,
-                      OverviewItem(
-                        textBig: "18",
-                        text: "days workout",
-                        icon: FontAwesomeIcons.handFist,
-                        color: Colors.red.shade400,
-                        padding: itemPadding,
-                        onTap: () {},
-                      ),
-                      spacer,
-                      OverviewItem(
-                        textBig: "5",
-                        text: "days rest",
-                        icon: FontAwesomeIcons.moon,
-                        color: Colors.blueGrey.shade200,
-                        padding: itemPadding,
-                        onTap: () {},
-                      ),
-                      spacer,
-                      OverviewItem(
-                        textBig: "4",
-                        text: "events soon",
-                        icon: FontAwesomeIcons.calendarCheck,
-                        color: Colors.green.shade200,
-                        padding: itemPadding,
-                        onTap: () {},
-                      ),
-                    ],
+    return HomeCard(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: EdgeInsets.all(PredefinedSize.paddingSmall),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // Text(
+            //   "Overview",
+            //   style: theme.textTheme.titleMedium!.copyWith(
+            //     fontSize: 18,
+            //     color: theme.colorScheme.onPrimary,
+            //     fontWeight: FontWeight.bold,
+            //   ),
+            // ),
+            // SizedBox(height: PredefinedSize.padding),
+            ClipRRect(
+              borderRadius: borderRadius,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  OverviewItem(
+                    textBig: "6",
+                    text: 'days streak',
+                    icon: FontAwesomeIcons.fire,
+                    color: theme.colorScheme.primary,
+                    backgroundColor: theme.colorScheme.background.withOpacity(.7),
+                    padding: itemPadding,
+                    onTap: () {},
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: PredefinedSize.paddingSmall),
-                  child: Text(
-                    "More information",
-                    style: theme.textTheme.labelMedium!.copyWith(
-                      color: theme.colorScheme.onPrimary,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  spacer,
+                  OverviewItem(
+                    textBig: "18",
+                    text: "days workout",
+                    icon: FontAwesomeIcons.handFist,
+                    color: Colors.red.shade400,
+                    padding: itemPadding,
+                    onTap: () {},
                   ),
-                ),
-              ],
+                  spacer,
+                  OverviewItem(
+                    textBig: "5",
+                    text: "days rest",
+                    icon: FontAwesomeIcons.moon,
+                    color: Colors.blueGrey.shade200,
+                    padding: itemPadding,
+                    onTap: () {},
+                  ),
+                  spacer,
+                  OverviewItem(
+                    textBig: "4",
+                    text: "events soon",
+                    icon: FontAwesomeIcons.calendarCheck,
+                    color: Colors.green.shade200,
+                    padding: itemPadding,
+                    onTap: () {},
+                  ),
+                ],
+              ),
             ),
-          ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: PredefinedSize.paddingSmall),
+              child: Text(
+                "More information…",
+                style: theme.textTheme.labelMedium!.copyWith(
+                  color: theme.colorScheme.onPrimary,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
