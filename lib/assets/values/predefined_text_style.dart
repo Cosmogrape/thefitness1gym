09695f49e6ddade1c24a7df8bad4200d37db61b3
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-05 21:42:35 +0300

    feat: style of page title to be used across the app

---
+++ lib/assets/values/predefined_text_style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PredefinedTextStyle {
+  static TextStyle pageTitle(ThemeData theme) {
+    return theme.textTheme.headlineSmall!.copyWith(
+      color: theme.colorScheme.onBackground,
+      fontWeight: FontWeight.bold,
+    );
+  }
+}
