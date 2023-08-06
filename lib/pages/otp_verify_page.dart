3ee3af7b0b556eb1e6729c681da03496aa67100b
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-06 17:49:29 +0300

    feat(placeholder): button to go to HomePage

--- lib/pages/otp_verify_page.dart
+++ lib/pages/otp_verify_page.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
 
-class OtpVerifyPage extends StatelessWidget {
+import 'home_page.dart';
+
+class OtpVerifyPage extends StatefulWidget {
   const OtpVerifyPage({super.key});
 
+  static MaterialPageRoute get route => MaterialPageRoute(builder: (context) => const OtpVerifyPage());
+
+  @override
+  State<OtpVerifyPage> createState() => _OtpVerifyPageState();
+}
+
+class _OtpVerifyPageState extends State<OtpVerifyPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Hi'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Verification"),
+        centerTitle: true,
+        leading: const BackButton(),
+      ),
+      body: TextButton(
+        onPressed: () {
+          Navigator.of(context).push(HomePage.route);
+        },
+        child: const Text("Go to home (placeholder"),
+      ),
     );
   }
 }
