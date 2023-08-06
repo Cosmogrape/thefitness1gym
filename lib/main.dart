839ce700ba5f06e0c96653c68d49f035e1b25e4d
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-06 13:02:10 +0300

    remove(temp): disable firebase auth for now

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:thefitness1gym/app.dart';
-import 'firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
 
   runApp(const App());
 }
