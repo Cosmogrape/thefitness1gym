7e040f045f6cf1384d4a3e5689b36bbf6ae3b2d8
Flacial <ruthemiumwithdiamond@protonmail.com>
2023-08-07 05:40:42 +0400

    feat: Complete OTP page (demo design)

--- lib/pages/otp_verify_page.dart
+++ lib/pages/otp_verify_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
+import 'package:thefitness1gym/assets/values/predefined_size.dart';
 
 import 'home_page.dart';
 
@@ -12,20 +14,80 @@ class OtpVerifyPage extends StatefulWidget {
 }
 
 class _OtpVerifyPageState extends State<OtpVerifyPage> {
+  final defaultPinTheme = PinTheme(
+      width: 56,
+      height: 60,
+      textStyle: const TextStyle(fontSize: 20, color: Colors.amber, fontWeight: FontWeight.w600),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.amber),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      margin: const EdgeInsets.all(PredefinedSize.small));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Verification"),
-        centerTitle: true,
         leading: const BackButton(),
+        backgroundColor: Colors.transparent,
+        title: Text(
+          'Verify your phone',
+          style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                color: Colors.white,
+              ),
+        ),
+        centerTitle: true,
       ),
-      body: TextButton(
-        onPressed: () {
-          Navigator.of(context).push(HomePage.route);
-        },
-        child: const Text("Go to home (placeholder"),
-      ),
+      body: SafeArea(
+          child: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: PredefinedSize.big + 100,
+            ),
+            Column(
+              children: [
+                Text(
+                  'Verify your\n Phone number',
+                  style: Theme.of(context).textTheme.headlineMedium!.copyWith(color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'Enter your OTP code here',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                )
+              ],
+            ),
+            const SizedBox(height: PredefinedSize.large),
+            Pinput(
+              onCompleted: (pin) async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomePage(),
+                  ),
+                );
+              },
+              length: 4,
+              defaultPinTheme: defaultPinTheme,
+            ),
+            const SizedBox(height: PredefinedSize.large),
+            Text(
+              "Didn't receive any code?",
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            const SizedBox(height: PredefinedSize.small),
+            Text(
+              "RESEND A NEW CODE",
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium!
+                  .copyWith(color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
