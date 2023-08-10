4bc54520aee86e0b2423b2b821bebb1cab7146a3
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-10 23:20:42 +0300

    update: remove padding to match all items

--- lib/widgets/profile_widgets/your_information.dart
+++ lib/widgets/profile_widgets/your_information.dart
@@ -1,7 +1,6 @@
 import 'package:date_time_format/date_time_format.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:thefitness1gym/assets/values/predefined_padding.dart';
 import 'package:thefitness1gym/global/widgets/map_list.dart';
 import 'package:thefitness1gym/widgets/profile_widgets/profile_item.dart';
 
@@ -21,23 +20,20 @@ class YourInformation extends StatelessWidget {
       // initiallyExpanded: true,
       icon: FontAwesomeIcons.solidUser,
       header: Text("Your information", style: headerStyle),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: PredefinedPadding.small, horizontal: PredefinedPadding.regular),
-        child: MapList(
-          map: {
-            "Name": "John Cena",
-            "Address": "Kathmandu, Nepal",
-            "Email": "therealjohn0_99@gmail.com",
-            "DoB": DateTime(1970, 12, 31).format("d/m/Y"),
-            "Gender": "Male",
-            "Phone": "+999 123123456",
-          },
-          keyStyle: theme.textTheme.bodyLarge!.copyWith(
-            color: theme.colorScheme.onBackground,
-            fontWeight: FontWeight.bold,
-          ),
-          valueStyle: theme.textTheme.bodyLarge,
+      body: MapList(
+        map: {
+          "Name": "John Cena",
+          "Address": "Kathmandu, Nepal",
+          "Email": "therealjohn0_99@gmail.com",
+          "DoB": DateTime(1970, 12, 31).format("d/m/Y"),
+          "Gender": "Male",
+          "Phone": "+999 123123456",
+        },
+        keyStyle: theme.textTheme.bodyLarge!.copyWith(
+          color: theme.colorScheme.onBackground,
+          fontWeight: FontWeight.bold,
         ),
+        valueStyle: theme.textTheme.bodyLarge,
       ),
     );
   }
