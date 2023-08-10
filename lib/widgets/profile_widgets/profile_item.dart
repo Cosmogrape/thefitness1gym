4a7c28cfef9e143c724093427645f0b0f6fde56a
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-10 15:52:53 +0300

    feat: item for ProfilePage (uses `ExpandablePanel`)

---
+++ lib/widgets/profile_widgets/profile_item.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:thefitness1gym/assets/values/predefined_padding.dart';
+import 'package:thefitness1gym/global/color_extension.dart';
+import 'package:thefitness1gym/global/widgets/expandable_panel.dart';
+
+class ProfileItem extends StatelessWidget {
+  const ProfileItem({
+    super.key,
+    required this.header,
+    required this.body,
+    this.color,
+    this.icon,
+    this.initiallyExpanded = false,
+  });
+
+  final Widget header;
+  final Widget body;
+  final Color? color;
+  final IconData? icon;
+  final bool initiallyExpanded;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    const borderRadius = PredefinedPadding.medium;
+    const headerPadding = EdgeInsets.all(PredefinedPadding.medium);
+    const bodyPadding = EdgeInsets.only(
+      top: PredefinedPadding.regular,
+      bottom: PredefinedPadding.medium,
+      left: PredefinedPadding.medium,
+      right: PredefinedPadding.medium,
+    );
+    const double iconSize = 24;
+    final headerBackgroundColor = theme.colorScheme.surface;
+    final bodyBackgroundColor = theme.colorScheme.surface.multiply(.5);
+    return ExpandablePanel(
+      initiallyExpanded: initiallyExpanded,
+      useInkWell: true,
+      borderRadius: borderRadius,
+      color: color ?? theme.colorScheme.primary,
+      iconSize: iconSize,
+      headerPadding: headerPadding,
+      bodyPadding: bodyPadding,
+      headerBackgroundColor: headerBackgroundColor,
+      bodyBackgroundColor: bodyBackgroundColor,
+      icon: icon,
+      header: header,
+      body: body,
+    );
+  }
+}
