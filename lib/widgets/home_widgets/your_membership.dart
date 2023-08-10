ac8dc93cd8563c5754043b1d2ef809850011d34f
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-10 15:18:36 +0300

    remove: content animations

--- lib/widgets/home_widgets/your_membership.dart
+++ lib/widgets/home_widgets/your_membership.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 
-import 'package:animated_size_and_fade/animated_size_and_fade.dart';
 import 'package:flutter/material.dart';
 import 'package:thefitness1gym/assets/values/predefined_padding.dart';
 import 'package:thefitness1gym/widgets/home_widgets/home_card.dart';
@@ -13,21 +12,6 @@ class YourMembership extends StatefulWidget {
 }
 
 class _YourMembershipState extends State<YourMembership> {
-  bool showContent = false;
-
-  void _resetAnimation() {
-    showContent = false;
-    Future.delayed(const Duration(milliseconds: 750), () {
-      setState(() => showContent = true);
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _resetAnimation();
-  }
-
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -35,7 +19,7 @@ class _YourMembershipState extends State<YourMembership> {
     //? Not using HomeCard because of the masked image on the right
     return HomeCard(
       heroTag: "membership",
-      onTap: _resetAnimation,
+      // onTap: _resetAnimation,
       color: theme.colorScheme.onBackground,
       child: ClipRRect(
         borderRadius: BorderRadius.circular(PredefinedPadding.medium),
@@ -48,19 +32,11 @@ class _YourMembershipState extends State<YourMembership> {
               ),
               child: Flexible(
                 // width: PredefinedPadding.medium,
-                child: AnimatedSizeAndFade.showHide(
-                  show: showContent,
-                  fadeDuration: const Duration(seconds: 1, milliseconds: 500),
-                  sizeDuration: const Duration(seconds: 1),
-                  fadeInCurve: Curves.easeInOut,
-                  fadeOutCurve: Curves.easeInOut,
-                  sizeCurve: Curves.easeInOut,
-                  child: Text(
-                    "Your Membership",
-                    style: theme.textTheme.titleLarge!.copyWith(
-                      color: theme.colorScheme.background,
-                      fontWeight: FontWeight.w600,
-                    ),
+                child: Text(
+                  "Your Membership",
+                  style: theme.textTheme.titleLarge!.copyWith(
+                    color: theme.colorScheme.background,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
               ),
@@ -69,16 +45,11 @@ class _YourMembershipState extends State<YourMembership> {
               right: 40,
               child: Transform(
                 transform: Matrix4.rotationZ(-pi / 6), //? -30° rotation
-                child: AnimatedOpacity(
-                  duration: const Duration(seconds: 1),
-                  curve: Curves.easeInOut,
-                  opacity: showContent ? 1 : 0,
-                  child: Image.asset(
-                    "lib/assets/images/dumbbell_man.png",
-                    alignment: Alignment.topCenter,
-                    fit: BoxFit.cover,
-                    width: 150,
-                  ),
+                child: Image.asset(
+                  "lib/assets/images/dumbbell_man.png",
+                  alignment: Alignment.topCenter,
+                  fit: BoxFit.cover,
+                  width: 150,
                 ),
               ),
             ),
