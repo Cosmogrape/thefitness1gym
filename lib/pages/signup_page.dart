19afe08302bb88034c2182f9a885a50cf7cb5a49
Flacial <ruthemiumwithdiamond@protonmail.com>
2023-08-11 10:01:35 +0400

    feat: Merge Signup and PhoneAuth pages

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:thefitness1gym/assets/values/predefined_padding.dart';
-
-// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-// import 'package:thefitness1gym/widgets/signup_box.dart';
+import 'package:thefitness1gym/assets/values/predefined_radius.dart';
 
 import '../widgets/fitness1_title.dart';
-import 'phone_auth_page.dart';
+import 'otp_verify_page.dart';
 
 @immutable
 class SignupPage extends StatefulWidget {
@@ -20,6 +18,7 @@ class SignupPage extends StatefulWidget {
 class _SignupPageState extends State<SignupPage> {
   bool nextEnabled = false;
   bool splashEnd = false;
+  bool onPhoneNumber = false;
 
   @override
   void initState() {
@@ -29,102 +28,175 @@ class _SignupPageState extends State<SignupPage> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            opacity: 0.6,
-            image: AssetImage('lib/assets/images/hot_man.png'),
-          ),
-        ),
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(32),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  alignment: Alignment.centerLeft,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Fitness1Title(),
-                      Text(
-                        'Unleash Your Greatness Today!',
-                        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                              fontWeight: FontWeight.w500,
-                            ),
-                      ),
-                    ],
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Sign in',
-                      style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                    ),
-                    const SizedBox(height: 10),
-                    Container(
-                        padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 30),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.black54,
-                        ),
-                        width: double.infinity,
-                        child: Column(
-                          children: [
-                            ElevatedButton(
-                              onPressed: () {
-                                Navigator.of(context).push(PhoneAuthPage.route);
-                              },
-                              style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all(Colors.amber),
-                                shape: MaterialStateProperty.all(
-                                  RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                ),
-                                minimumSize: MaterialStateProperty.all(const Size.fromHeight(50)),
-                              ),
-                              child: const Text(
-                                'Phone number',
-                                style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            const SizedBox(height: PredefinedPadding.small),
-                            ElevatedButton(
-                              onPressed: () {},
-                              style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all(Colors.white12),
-                                shape: MaterialStateProperty.all(
-                                  RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                ),
-                                minimumSize: MaterialStateProperty.all(const Size.fromHeight(50)),
-                              ),
-                              child: const Text(
-                                'Fingerprint ID',
-                                style: TextStyle(color: Colors.white38),
-                              ),
-                            ),
-                          ],
-                        ))
-                  ],
-                )
-              ],
+  final myController = TextEditingController();
+
+  SignInChildren() {
+    return [
+      ElevatedButton(
+        onPressed: () {
+          // Navigator.of(context).push(PhoneAuthPage.route);
+          setState(() {
+            onPhoneNumber = true;
+          });
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.amber),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
             ),
           ),
+          minimumSize: MaterialStateProperty.all(const Size.fromHeight(50)),
+        ),
+        child: const Text(
+          'Phone number',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
       ),
+      const SizedBox(height: PredefinedPadding.small),
+      ElevatedButton(
+        onPressed: () {},
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.white12),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          minimumSize: MaterialStateProperty.all(const Size.fromHeight(50)),
+        ),
+        child: const Text(
+          'Fingerprint ID',
+          style: TextStyle(color: Colors.white38),
+        ),
+      ),
+    ];
+  }
+
+  PhoneAuthChildren() {
+    return [
+      ClipRRect(
+        borderRadius: BorderRadius.circular(PredefinedRadius.regular),
+        child: TextFormField(
+          controller: myController,
+          decoration: const InputDecoration(
+            border: InputBorder.none,
+            labelText: 'Enter your phone number',
+            filled: true,
+            fillColor: Colors.white10,
+            contentPadding:
+                EdgeInsets.symmetric(vertical: PredefinedPadding.small, horizontal: PredefinedPadding.medium),
+          ),
+          keyboardType: TextInputType.number,
+        ),
+      ),
+      const SizedBox(height: PredefinedPadding.medium),
+      ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).push(OtpVerifyPage.route);
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.amber),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(PredefinedRadius.regular),
+            ),
+          ),
+          minimumSize: MaterialStateProperty.all(const Size.fromHeight(50)),
+        ),
+        child: const Text(
+          'Send OTP',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+      )
+    ];
+  }
+
+  Heading(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Fitness1Title(),
+          Text(
+            'Unleash Your Greatness Today!',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  fontWeight: FontWeight.w500,
+                ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  ContentTitle() {
+    return Row(children: [
+      Container(
+          child: onPhoneNumber
+              ? BackButton(onPressed: () {
+                  setState(() {
+                    onPhoneNumber = false;
+                  });
+                  myController.text = '';
+                })
+              : null),
+      Text(
+        onPhoneNumber ? 'Verify phone' : 'Sign in',
+        style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+      )
+    ]);
+  }
+
+  Content() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ContentTitle(),
+        const SizedBox(height: 10),
+        AnimatedSize(
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.ease,
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 30),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.black54,
+            ),
+            width: double.infinity,
+            child: Column(
+              children: onPhoneNumber ? PhoneAuthChildren() : SignInChildren(),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          opacity: 0.6,
+          image: AssetImage('lib/assets/images/hot_man.png'),
+        ),
+      ),
+      child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(32),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [Heading(context), Content()],
+              ),
+            ),
+          )),
     );
   }
 }
