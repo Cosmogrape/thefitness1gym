e1c0cf8784ba8747cfddfff98d0f733d08af8554
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-12 23:27:59 +0300

    feat: more padding values for bigger stuff

--- lib/values/predefined_padding.dart
+++ lib/values/predefined_padding.dart
@@ -10,4 +10,19 @@ class PredefinedPadding {
   static const double largeX = 30;
   static const double largeXX = 35;
   static const double huge = 40;
+  static const double hugeX = 45;
+  static const double hugeXX = 50;
+  static const double massive = 55;
+  static const double massiveX = 60;
+  static const double massiveXX = 65;
+  static const double yomama = 70;
+  static const double yomamaX = 75;
+  static const double yomamaXX = 80;
+  static const double yomamaXXX = 85;
+  static const double yomamaXXXX = 90;
+  static const double yomamaXXXXX = 95;
+  static const double yomamaXXXXXX = 100;
+  static const double yomamaZ = 200;
+  static const double yomamaZZ = 300;
+  static const double yomamaZZZ = 300;
 }
