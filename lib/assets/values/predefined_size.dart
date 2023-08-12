3bf594e2fc881603c512a943189cc9b752fa2058
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-12 17:08:20 +0300

    feat: button and toolbar height

--- lib/assets/values/predefined_size.dart
+++ lib/assets/values/predefined_size.dart
@@ -1,4 +1,8 @@
 class PredefinedSize {
   /// Default size of floating action button
   static const double fab = 56;
+
+  static const double buttonHeight = 50;
+
+  static const double toolbarHeight = 64;
 }
