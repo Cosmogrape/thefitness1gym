a1ab590224cea8892d5304bdf026fdc6b217eadf
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-12 17:44:27 +0300

    move: values from lib/assets/ to lib/

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:thefitness1gym/assets/values/predefined_misc.dart';
+import 'package:thefitness1gym/values/predefined_misc.dart';
 
 import 'pages/signup_page.dart';
 
