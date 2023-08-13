a963bf710af37cc425706c69904a7914d75c386b
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-13 18:43:51 +0300

    feat: skills of a coach

---
+++ lib/models/coach_skill.dart
@@ -0,0 +1,9 @@
+enum CoachSkill {
+  bodybuilding("Bodybuilding"),
+  fitnessTraining("Fitness Training"),
+  healthCoaching("Health Coaching"),
+  zombaTraining("Zomba Training");
+
+  final String name;
+  const CoachSkill(this.name);
+}
