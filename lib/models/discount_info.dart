2d730617af6ead4e666edf4370fe386a8805f19a
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-15 17:01:02 +0300

    feat: formatValue

--- lib/models/discount_info.dart
+++ lib/models/discount_info.dart
@@ -1,14 +1,23 @@
+import 'package:intl/intl.dart';
+
 class DiscountInfo {
   DiscountInfo({
     required this.value,
     required this.end,
-    this.isPercentage = true,
+    this.symbol,
   });
 
   final double value;
   final DateTime end;
-  final bool isPercentage;
+  final String? symbol;
 
-  @override
-  String toString() => "-$value${isPercentage ? '%' : ''}";
+  bool get isPercentage => symbol?.trim() == '%';
+
+  String formatValue({bool withSymbol = false}) {
+    final value = NumberFormat.decimalPatternDigits(
+      locale: "en_US",
+      decimalDigits: this.value % 1 == 0 ? 0 : 2,
+    ).format(this.value);
+    return "$value${withSymbol ? symbol : ''}";
+  }
 }
