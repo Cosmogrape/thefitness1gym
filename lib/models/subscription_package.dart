1db63a48236d723ab8fb6ca6e0b6256ddd41604d
Galacticai <24789792+Galacticai@users.noreply.github.com>
2023-08-15 17:01:32 +0300

    feat: formatPrice, discountedPrice, SubscriptionPackageType

--- lib/models/subscription_package.dart
+++ lib/models/subscription_package.dart
@@ -1,9 +1,20 @@
+import 'package:intl/intl.dart';
 import 'package:thefitness1gym/models/discount_info.dart';
+
+enum SubscriptionPackageType {
+  membership("Membership"),
+  personalTraining("Personal Training");
+
+  const SubscriptionPackageType(this.name);
+  final String name;
+}
 
 /// This (and similar stuff) will be models in order to act as proxy for the data from the server.
 /// So the data will be fetched from the server and then parsed into this model.
 class SubscriptionPackage {
   SubscriptionPackage({
+    required this.id,
+    required this.type,
     required this.duration,
     required this.price,
     required this.currency,
@@ -11,9 +22,26 @@ class SubscriptionPackage {
     this.perks,
   });
 
+  final String id;
+  final SubscriptionPackageType type;
   final Duration duration;
   final double price;
   final String currency;
   final DiscountInfo? discount;
   final List<String>? perks;
+
+  double get discountedPrice {
+    if (discount == null) return price;
+    return price - (discount!.isPercentage ? price * (discount!.value / 100) : discount!.value);
+  }
+
+  String formatPrice({bool withCurrency = false, bool withDiscount = false, String joint = ''}) {
+    final targetValue = withDiscount ? discountedPrice : price;
+    final value = NumberFormat.currency(
+      customPattern: "#,###.##",
+      locale: "en_US",
+      decimalDigits: targetValue % 1 == 0 ? 0 : 2,
+    ).format(targetValue);
+    return value + joint + (withCurrency ? currency : '');
+  }
 }
